f51762e3706a4ef5ced82acbf370d1db23445388
Nero Argento <drakonobor@gmail.com>
2024-03-16 22:29:24 +0300

    added more keys for models

--- auctions/models.py
+++ auctions/models.py
@@ -9,16 +9,16 @@ class Auction(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField(max_length=1024)
     starting_bid = models.IntegerField()
-    img_url = models.CharField(max_length=1024)
-    #owner = models.ForeignKey('User')
+    img_url = models.URLField(max_length=1024)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Bids(models.Model):
-    #title = models.ForeignKey("Auction", on_delete=models.CASCADE)
-    #starting_bid = models.ForeignKey("Auction.starting_bid", on_delete=models.CASCADE)
+    title = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    starting_bid = models.ForeignKey(Auction, on_delete=models.CASCADE)
     current_bid = models.IntegerField()
-    #bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Comments(models.Model):
-    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid_id = models.ForeignKey(Bids, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=1024)
-    #auction = models.ForeignKey()
